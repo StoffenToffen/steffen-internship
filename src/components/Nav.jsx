b31eebb58977556cbf4960d36f779b56e0df4790
Steffen Tveit <steffentveit@outlook.com>
2026-01-17 01:18:46 +0100

    Ran linter on every page, made SelectedCollection dynamic, and added loading state.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import Navbar from "./Navbar";
 import Menu from "./Menu";
+import Navbar from "./Navbar";
 
 export default function Nav() {
-  const [menuOpen, setMenuOpen] = useState(false);
+	const [menuOpen, setMenuOpen] = useState(false);
 
-  return (
-    <>
-      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-    </>
-  );
+	return (
+		<>
+			<Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+			<Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+		</>
+	);
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,20 @@
 import React, { useEffect } from "react";
-import SelectedCollection from "../components/home/SelectedCollection.jsx";
 import NewCollections from "../components/home/NewCollections.jsx";
 import PopularCollections from "../components/home/PopularCollections.jsx";
+import SelectedCollection from "../components/home/SelectedCollection.jsx";
 import Trending from "../components/home/Trending.jsx";
 
 export default function HomePage() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
 
-  return (
-    <>
-      <SelectedCollection />
-      <Trending />
-      <NewCollections />
-      <PopularCollections />
-    </>
-  );
+	return (
+		<>
+			<SelectedCollection />
+			<Trending />
+			<NewCollections />
+			<PopularCollections />
+		</>
+	);
 }
