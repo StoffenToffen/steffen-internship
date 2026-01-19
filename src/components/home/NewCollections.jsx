e3aed86c49501cdc89feb88c178c0a7c07910322
Steffen Tveit <steffentveit@outlook.com>
2026-01-20 00:25:17 +0100

    changed new-collection section to have a skeleton loading state

--- src/components/home/NewCollections.jsx
+++ src/components/home/NewCollections.jsx
@@ -7,6 +7,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 
+import CollectionsSkeleton from "../ui/CollectionsSkeleton";
+
 export default function NewCollections() {
 	const [newCollections, setNewCollections] = useState();
 
@@ -29,7 +31,7 @@ export default function NewCollections() {
 							modules={[Navigation]}
 							spaceBetween={30}
 							slidesPerView={1}
-							loop={newCollections}
+							loop
 							navigation
 							breakpoints={{
 								480: {
@@ -49,43 +51,58 @@ export default function NewCollections() {
 								},
 							}}
 						>
-							{newCollections?.map(
-								({ floor, title, imageLink, totalVolume, collectionId }) => (
-									<SwiperSlide className="collection-column" key={collectionId}>
-										<Link
-											to={`/collection/${collectionId}`}
-											className="collection"
-										>
-											<img
-												src={imageLink}
-												alt={title}
-												className="collection__img"
-											/>
-											<div className="collection__info">
-												<h3 className="collection__name">{title}</h3>
-												<div className="collection__stats">
-													<div className="collection__stat">
-														<span className="collection__stat__label">
-															Floor
-														</span>
-														<span className="collection__stat__data">
-															{parseFloat(floor).toFixed(2)} ETH
-														</span>
+							{newCollections
+								? newCollections.map(
+										({
+											floor,
+											title,
+											imageLink,
+											totalVolume,
+											collectionId,
+										}) => (
+											<SwiperSlide
+												className="collection-column"
+												key={collectionId}
+											>
+												<Link
+													to={`/collection/${collectionId}`}
+													className="collection"
+												>
+													<img
+														src={imageLink}
+														alt={title}
+														className="collection__img"
+													/>
+													<div className="collection__info">
+														<h3 className="collection__name">{title}</h3>
+														<div className="collection__stats">
+															<div className="collection__stat">
+																<span className="collection__stat__label">
+																	Floor
+																</span>
+																<span className="collection__stat__data">
+																	{parseFloat(floor).toFixed(2)} ETH
+																</span>
+															</div>
+															<div className="collection__stat">
+																<span className="collection__stat__label">
+																	Total Volume
+																</span>
+																<span className="collection__stat__data">
+																	{totalVolume} ETH
+																</span>
+															</div>
+														</div>
 													</div>
-													<div className="collection__stat">
-														<span className="collection__stat__label">
-															Total Volume
-														</span>
-														<span className="collection__stat__data">
-															{totalVolume} ETH
-														</span>
-													</div>
-												</div>
-											</div>
-										</Link>
-									</SwiperSlide>
-								),
-							)}
+												</Link>
+											</SwiperSlide>
+										),
+									)
+								: new Array(9).fill(0).map((_, index) => (
+										<SwiperSlide key={index} className="collection-column">
+											<CollectionsSkeleton />
+										</SwiperSlide>
+									))}
 						</Swiper>
 					</div>
 				</div>
---
+++ src/components/ui/CollectionsSkeleton.jsx
@@ -0,0 +1,31 @@
+import Skeleton from "./Skeleton.jsx";
+
+const CollectionsSkeleton = () => {
+	return (
+		<div className="collection">
+			<Skeleton width="500px" height="200px" />
+			<div className="collection__info">
+				<div className="collection__stats">
+					<div className="collection__stat">
+						<span className="collection__stat__label">
+							<Skeleton width="40px" height="20px" borderRadius="4px" />
+						</span>
+						<span className="collection__stat__data">
+							<Skeleton width="80px" height="20px" borderRadius="4px" />
+						</span>
+					</div>
+					<div className="collection__stat">
+						<span className="collection__stat__label">
+							<Skeleton width="40px" height="20px" borderRadius="4px" />
+						</span>
+						<span className="collection__stat__data">
+							<Skeleton width="80px" height="20px" borderRadius="4px" />
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default CollectionsSkeleton;
