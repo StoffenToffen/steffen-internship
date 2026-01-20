1bf09c726dbd06f5c046eb275df350ffeaf69000
Steffen Tveit <steffentveit@outlook.com>
2026-01-20 13:14:06 +0100

    changed popular-collections section to be identical to new-collections, just with a different api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,35 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
+
 import NewCollections from "../components/home/NewCollections.jsx";
 import PopularCollections from "../components/home/PopularCollections.jsx";
 import SelectedCollection from "../components/home/SelectedCollection.jsx";
 import Trending from "../components/home/Trending.jsx";
 
 export default function HomePage() {
+	const swiperSettingsCollections = {
+		spaceBetween: 30,
+		slidesPerView: 1,
+		loop: true,
+		navigation: true,
+		breakpoints: {
+			480: {
+				slidesPerView: 2,
+			},
+			768: {
+				slidesPerView: 3,
+			},
+			1024: {
+				slidesPerView: 4,
+			},
+			1200: {
+				slidesPerView: 5,
+			},
+			1600: {
+				slidesPerView: 6,
+			},
+		},
+	};
+
 	useEffect(() => {
 		window.scrollTo(0, 0);
 	}, []);
@@ -13,8 +38,8 @@ export default function HomePage() {
 		<>
 			<SelectedCollection />
 			<Trending />
-			<NewCollections />
-			<PopularCollections />
+			<NewCollections swiperSettings={swiperSettingsCollections} />
+			<PopularCollections swiperSettings={swiperSettingsCollections} />
 		</>
 	);
 }
